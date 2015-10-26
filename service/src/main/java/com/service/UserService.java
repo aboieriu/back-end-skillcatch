c5770d1deb90c4d3e4cb76dfe09535dd93de6c26
CatalinVlad <cata_bv94@yahoo.com>
2015-10-26 12:49:28 +0200

    ServiceNoGen

---
+++ service/src/main/java/com/service/UserService.java
@@ -0,0 +1,75 @@
+package com.service;
+
+import model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * Created by CataVlad on 26-Oct-15.
+ */
+@Controller
+public class UserService {
+
+    @Autowired
+    IUserFacade userFacade;
+
+    @RequestMapping(value = "/api/group/{groupId}/user" , method = RequestMethod.GET)
+    @ResponseBody
+    public List<User> getAllUser(Long id)
+    {
+        return this.userFacade.getAllUser(id);
+    }
+
+    @RequestMapping(value = "/api/group/{groupId}/user/{userId}",method = RequestMethod.GET)
+    @ResponseBody
+    public User getUser(@PathVariable("groupId") Long groupId , @PathVariable("userId") Long userId)
+    {
+        return this.userFacade.getUser(groupId, userId);
+    }
+
+    @RequestMapping(value = "/api/group/{groupId}/user/{userId}",method = RequestMethod.DELETE)
+    @ResponseBody
+    public void deleteUser(@PathVariable("groupId") Long groupId , @PathVariable("userId") Long userId)
+    {
+        return this.userFacade.deleteUser(groupId, userId);
+    }
+
+    @RequestMapping(value = "/api/group/{groupId}/user/" , method = RequestMethod.POST)
+    @ResponseBody
+    public void addUser(@PathVariable("groupId") Long groupId,@RequestBody User userId)
+    {
+        userId.setGroupId(groupId);
+        return this.userFacade.addUser(userId);
+    }
+
+    @RequestMapping(value = "/api/group/{groupId}/user/{userId}", method = RequestMethod.PUT)
+    @ResponseBody
+    public void updateUser(@PathVariable("userId") Long id ,@PathVariable("groupId") Long groupId,@RequestBody User user) {
+        user.setId(id);
+        user.setGroupId(groupId);
+        this.userFacade.updateUser(user);
+    }
+
+    public IUserFacade getUserFacade() {
+        return userFacade;
+    }
+
+    public void setUserFacade(IUserFacade userFacade) {
+        this.userFacade = userFacade;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
