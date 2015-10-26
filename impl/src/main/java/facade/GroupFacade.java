7b398fb5bad56c4c9db83d9d3332f53e9169fa62
AdrianaCatoiu <adriana.catoiu@yahoo.com>
2015-10-26 12:19:37 +0200

    Facade (Group, User) - no gen.

---
+++ impl/src/main/java/facade/GroupFacade.java
@@ -0,0 +1,45 @@
+package facade;
+
+import dao.IGroupDao;
+import model.Group;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+/**
+ * Created by Adi on 10/26/2015.
+ */
+public class GroupFacade implements IGroupFacade{
+
+    @Autowired
+    private IGroupDao groupDao;
+
+    public IGroupDao getGroupDao() {
+        return groupDao;
+    }
+
+    public void setGroupDao(IGroupDao groupDao) {
+        this.groupDao = groupDao;
+    }
+
+    public List<Group> getAllGroup(){
+        return this.groupDao.getAllGroup();
+            }
+
+    public Group getGroup(Long groupId){
+    return this.groupDao.getGroup(groupId);}
+
+    public void addGroup(Group group){
+        this.groupDao.addGroup(group);
+    }
+
+    public void deleteGroup(Long groupId){
+        this.groupDao.deleteGroup(groupId);
+    }
+
+    public void updateGroup(Group group){
+        this.groupDao.updateGroup(group);
+    }
+
+
+}
---
+++ impl/src/main/java/facade/UserFacade.java
@@ -0,0 +1,52 @@
+package facade;
+
+import dao.IUserDao;
+import model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+/**
+ * Created by Adi on 10/26/2015.
+ */
+public class UserFacade implements IUserFacade{
+
+    @Autowired
+    private IUserDao userDao;
+
+    public IUserDao getUserDao() {
+        return userDao;
+    }
+
+    public void setUserDao(IUserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    public List<User> getAllUser(Long groupId)
+    {
+        return this.userDao.getAllUsers(groupId);
+    }
+
+    public User getUser(Long groupId, Long userId)
+    {
+        return this.userDao.getUser(groupId,userId);
+    }
+
+    public void deleteUser(Long groupId, Long userId)
+    {
+        this.userDao.deleteUser(groupId, userId);
+    }
+
+    public void addUser(User group)
+    {
+        this.userDao.addUser(group);
+    }
+
+    public void updateUser(User group)
+    {
+        this.userDao.updateUser(group);
+    }
+
+
+
+}
