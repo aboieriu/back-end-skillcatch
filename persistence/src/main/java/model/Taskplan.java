99ae2f55e615ef89b5e98ee44f7968daf12ab0fa
Ionut <cringanu_ionutz@yahoo.ro>
2015-11-20 11:55:57 +0200

    update Master+Login

--- persistence/src/main/java/model/Taskplan.java
+++ persistence/src/main/java/model/Taskplan.java
@@ -1,7 +1,50 @@
 package model;
+import org.hibernate.annotations.GenericGenerator;
 
-/**
- * Created by Johnny on 20.11.2015.
- */
+import javax.persistence.*;
+
+
+@Entity
+@Table(name="task_plan")
 public class Taskplan {
+
+    @GeneratedValue(generator = "idIncrementor")
+    @GenericGenerator(name = "idIncrementor", strategy = "increment")
+    @Id
+    private Long taskPlanId;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    public Taskplan(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public Long getTaskPlanId() {
+        return taskPlanId;
+    }
+
+    public void setTaskPlanId(Long taskPlanId) {
+        this.taskPlanId = taskPlanId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
