69ea4de1a46e4c5d608ddc500a082b4d33cbba7f
musatandrei <musatandrei22@yahoo.com>
2016-02-29 08:45:36 +0200

    2

---
+++ persistence/src/main/java/model/ProjectGroup.java
@@ -0,0 +1,106 @@
+package model;
+
+import org.hibernate.annotations.*;
+
+import javax.persistence.*;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.ForeignKey;
+import javax.persistence.Table;
+import java.util.Collection;
+import java.util.Date;
+import java.util.Set;
+
+
+@Entity
+@Table(name = "project_group")
+public class ProjectGroup {
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @GeneratedValue(generator = "idIncrementor")
+    @GenericGenerator(name = "idIncrementor", strategy = "increment")
+    @Id
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "descriptions")
+    private String descriptions;
+
+    @Column(name = "status")
+    private Long status;
+
+    public ProjectGroup() {}
+
+    @ManyToMany(cascade = {CascadeType.ALL},fetch = FetchType.EAGER)
+    @JoinTable(name="project_group_has_user",
+            joinColumns={@JoinColumn(name="project_group_id")},
+            inverseJoinColumns = {@JoinColumn(name="user_id")})
+    private Set<User> users;
+
+    @ManyToMany(cascade = {CascadeType.ALL},fetch = FetchType.EAGER)
+
+    @JoinTable(name="project_group_has_task_plan",
+            joinColumns={@JoinColumn(name="project_group_id")},
+            inverseJoinColumns = {@JoinColumn(name="task_plan_id")})
+
+    private Set<Taskplan> taskplans;
+
+    public Set<Taskplan> getTaskplans() {
+        return taskplans;
+    }
+
+    public void setTaskplans(Set<Taskplan> taskplans) {
+        this.taskplans = taskplans;
+    }
+
+    public ProjectGroup(String name, String descriptions, Long status, Set<User> users, Set<Taskplan> taskplans) {
+        this.name = name;
+        this.descriptions = descriptions;
+        this.status = status;
+        this.users = users;
+        this.taskplans = taskplans;
+    }
+
+    public Set<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(Set<User> users) {
+        this.users = users;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescriptions() {
+        return descriptions;
+    }
+
+    public void setDescriptions(String descriptions) {
+        this.descriptions = descriptions;
+    }
+
+    public Long getStatus() {
+        return status;
+    }
+
+    public void setStatus(Long status) {
+        this.status = status;
+    }
+
+}
