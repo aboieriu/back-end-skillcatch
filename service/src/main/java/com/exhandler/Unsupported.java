7bf560d6e924b48538995a4743ca760096658796
Cringanu Ionut <cringanu_ionutz@yahoo.ro>
2016-03-18 10:47:49 +0200

    2

---
+++ service/src/main/java/com/exhandler/Unsupported.java
@@ -0,0 +1,12 @@
+package com.exhandler;
+
+/**
+ * Created by icringanu on 04.03.2016.
+ */
+public class Unsupported extends RuntimeException {
+
+    public Unsupported(String mesaj){
+
+        super(mesaj);
+    }
+}
