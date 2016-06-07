9b6521e84c344e5fe88dbc2e7211ad9e975ca29c
musatandrei <musatandrei22@yahoo.com>
2016-06-07 17:08:16 +0300

    User details

---
+++ service/src/main/java/com/converter/AssignedProjectConverter.java
@@ -0,0 +1,7 @@
+package com.converter;
+
+/**
+ * Created by amusat on 6/7/2016.
+ */
+public class AssignedProjectConverter {
+}
---
+++ service/src/main/java/com/view/AssignedProjectView.java
@@ -0,0 +1,7 @@
+package com.view;
+
+/**
+ * Created by amusat on 6/7/2016.
+ */
+public class AssignedProjectView {
+}
