e6a5c4eb526e9faae4cac1bc6e3825d49fc18c13
Cringanu Ionut <cringanu_ionutz@yahoo.ro>
2016-08-29 14:44:22 +0300

    Updated Persistence with issues solved

---
+++ service/persistence/src/main/java/dao/api/ITaskDao.java
@@ -0,0 +1,21 @@
+package dao.api;
+
+import model.Badge;
+import model.Task;
+
+import java.util.List;
+
+
+public interface ITaskDao extends IGenericDao<Task>  {
+
+    public Task getTask(Long groupId , Long taskplanId, Long taskId);
+    public Task getUserTask(Long groupId ,Long taskId);
+    void updateTask(Task task);
+    public List<Badge> getBadgeFromTask(Long taskId);
+    public void add(Task task);
+
+    void deleteById(Long taskId);
+
+
+
+}
---
+++ service/service/src/main/java/com/service/CorsFilter.java
@@ -0,0 +1,35 @@
+package com.service;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class CorsFilter implements Filter {
+    public void destroy() {
+        // TODO Auto-generated method stub
+    }
+
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain)
+            throws IOException, ServletException {
+        // TODO Auto-generated method stub
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+        if (httpServletRequest.getHeaders("origin").hasMoreElements()) {
+            httpServletResponse.setHeader("Access-Control-Allow-Origin", httpServletRequest.getHeaders("origin").nextElement());
+        }
+        httpServletResponse.addHeader("Access-Control-Allow-Methods","POST, GET, OPTIONS, DELETE, PUT");
+        httpServletResponse.addHeader("Access-Control-Max-Age","3600");
+        httpServletResponse.addHeader("Access-Control-Allow-Headers", "x-requested-with, X-Auth-Token, Content-Type");
+        httpServletResponse.setHeader("Access-Control-Allow-Credentials", "true");
+        if (httpServletRequest.getMethod().equals("OPTIONS")) {
+            return;
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    public void init(FilterConfig arg0) throws ServletException {
+        // TODO Auto-generated method stub
+    }
+}
+
